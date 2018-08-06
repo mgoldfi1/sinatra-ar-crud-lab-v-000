bc898b7a8c3863aeedf1be142e180a165a6c8749
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-06 14:52:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   patch '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     @post.update(name: params[:name], content: params[:content])
-    @post.save
+
   end
 
 
